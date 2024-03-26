c7caed1e23f2b71273755aeb32474aaaacc535e9
Карпович Кирилл ИСТбд-22 <99400515+GL1ntve1n@users.noreply.github.com>
2024-03-26 16:36:22 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import re
+
+def file(filename):
+    with open(filename, 'r') as file:
+        data = file.read()
+
+    print("Все числа из файла")
+    print(data)
+
+    # Рег выражение
+    pattern = r'\b\d9\d*9[02468]\b'
+    numbers = re.findall(pattern, data)
+
+    print("Числа из файла подходящие под условия:")
+    for num in numbers:
+        print(num, end=" ")
+    print(" ")
+
+    if numbers:
+        min_number = min(map(int,numbers))
+        count = len(numbers)
+    else:
+        min_number = None
+        count = 0
+
+    return min_number, count
+
+filename = 'numbers.txt'
+min_number, count = file(filename)
+if min_number is not None:
+    print(f"Минимальное число: {min_number}")
+    print(f"Количество подходящих чисел: {count}")
+else:
+    print("В файле нет подходящих чисел")
